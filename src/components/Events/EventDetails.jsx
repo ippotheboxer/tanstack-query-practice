5c8eadbfe37ab88bd3b8c0e45e048b159573c3ee
Sophie <sophiesokol123@gmail.com>
2025-03-24 13:21:46 +0000

    Make deleting an event work and clean up EventDetails.jsx

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,18 +1,88 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, useNavigate, Outlet, useParams } from "react-router-dom";
 
 import Header from "../Header.jsx";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 
 export default function EventDetails() {
+  const navigate = useNavigate();
+
   let params = useParams();
-  let id = params.id
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["event-details", {id}],
-    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+    queryKey: ["events", { id: params.id }],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
+
+  const { mutate } = useMutation({
+    mutationFn: () => deleteEvent({ id: params.id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id: params.id });
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data...</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="An error occured"
+          message={
+            error.info?.message || "Failed to fetch details for the event."
+          }
+        />
+      </div>
+    );
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
@@ -22,29 +92,7 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      {isPending && <p style={{textAlign: 'center'}}>Loading data for this event...</p>}
-      {isError && <ErrorBlock title="An error occured" message={error.info?.message || "Failed to fetch details for the event."} />}
-      {data && (
-        <article id="event-details">
-          <header>
-            <h1>{data.title}</h1>
-            <nav>
-              <button>Delete</button>
-              <Link to="edit">Edit</Link>
-            </nav>
-          </header>
-          <div id="event-details-content">
-            <img src={`http://localhost:3000/${data.image}`} alt="Event image" />
-            <div id="event-details-info">
-              <div>
-                <p id="event-details-location">{data.location}</p>
-                <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} {data.time}</time>
-              </div>
-              <p id="event-details-description">{data.description}</p>
-            </div>
-          </div>
-        </article>
-      )}
+      <article id="event-details">{content}</article>
     </>
   );
 }
